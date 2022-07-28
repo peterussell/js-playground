acee1cb964e76d473e96c6a1a7665919a6f78c77
Pete Russell <pinkbatz@gmail.com>
2022-07-28 12:03:44 +1200

    Added more matcher examples.

--- jest-playground/src/sum.ts
+++ jest-playground/src/sum.ts
@@ -1,4 +1,12 @@
-const sum = (a: number, b: number): number => {
+const sum = (a: number, b: number): (number | undefined) => {
+  if (a < 0 || b < 0) {
+    return undefined;
+  }
+
+  if (a >= 100 || b >= 100) {
+    throw new Error("Values must be smaller than 100.");
+  }
+
   return a+b;
 };
 
--- jest-playground/test/sum.test.ts
+++ jest-playground/test/sum.test.ts
@@ -3,3 +3,15 @@ import sum from "../src/sum";
 test("adds 1 + 2 and returns 3", () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test("adds 1 + 2 and checks the result isn't 4", () => {
+  expect(sum(1, 2)).not.toBe(4);
+});
+
+test("adding negative numbers returns undefined", () => {
+  expect(sum(-1, 1)).toBeUndefined();
+});
+
+test("adding numbers greater than or equal to 100 throws an exception", () => {
+  expect(() => sum(100, 101)).toThrow("Values must be smaller than 100.");
+});
